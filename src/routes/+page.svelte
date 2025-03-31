2136e07bbc1bb349b88da91b7359013b78f12a5e
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2025-03-31 17:48:00 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,70 @@
 <script>
-  
+  import { onMount } from 'svelte';
+
+onMount(() => {
+    document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+  let prevCardHeight = 0;
+
+  cards.forEach((card, index) => {
+    if (index > 0) {
+      card.style.top = `${prevCardHeight}px`;
+      prevCardHeight += card.clientHeight; 
+    }
+  });
+});
+});
 </script>
 
 <section>
-<h2>Onze nieuwste boeken</h2>
-<!-- carousel -->
+  <h2>Onze nieuwste boeken</h2>
+  <!-- carousel -->
 </section>
 
 <section>
-<h2>Het laatste nieuws</h2>
+  <h2>Het laatste nieuws</h2>
 </section>
 
-<section>
-    <h2>beleef met oba</h2>
-    <!-- stick on scroll cards -->
+<section class="sticky">
+  <h2>beleef met oba</h2>
+  <div class="sticky_container">
+    <div class="card">Card 1</div>
+    <div class="card">Card 2</div>
+    <div class="card">Card 3</div>
+  </div>
 </section>
 
+<style>
+  .sticky {
+    min-height: fit-content; 
+    position: relative;
+  }
 
-<style scoped>
+  .sticky_container {
+    position: relative;
+    display: flex;
+    flex-flow: column;
+    gap: 2rem;
+    align-items: center;
+  }
 
+  .card {
+    position: sticky;
+    top: 0;
+    height: 70vh;
+    width: 100%;
+    background: #ccc;
+    display: flex;
+    font-size: 3rem;
+    border-radius: 0.5rem;
+  }
+  .card:nth-child(1) {
+    background: #777;
+  }
+  .card:nth-child(2) {
+    background: #555;
+  }
+  .card:nth-child(3) {
+    background: #333;
+  }
 </style>
