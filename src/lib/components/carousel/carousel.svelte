206d9ed5245dafc834045881bcfd423225edd28a
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2025-05-25 11:02:07 +0200

    Update carousel.svelte

--- src/lib/components/carousel/carousel.svelte
+++ src/lib/components/carousel/carousel.svelte
@@ -1,95 +1,95 @@
 <script>
-
 </script>
 
 <ul class="books_container">
-    <li>
-      <img
-        class="book_image"
-        src="https://leibniz.zbkb.nl/assets/id/PPN%3A436719541?aid=ob-oba-lokaal-waas&sid=14&dts=1743610608606&sig=c654736f76693c3d4d298d77057b84190c5780cdaa6ac7e3b712ab6ed82407cf&width=100"
-        alt=""
-      />
-      <div>
-        <a class="book_title" href="/">Laat me binnen</a>
-        <span class="author">Manon Uphoff</span>
-      </div>
-    </li>
+  <li>
+    <img
+      class="book_image"
+      src="https://leibniz.zbkb.nl/assets/id/PPN%3A436719541?aid=ob-oba-lokaal-waas&sid=14&dts=1743610608606&sig=c654736f76693c3d4d298d77057b84190c5780cdaa6ac7e3b712ab6ed82407cf&width=100"
+      alt=""
+    />
+    <div>
+      <a class="book_title" href="/">Laat me binnen</a>
+      <span class="author">Manon Uphoff</span>
+    </div>
+  </li>
 
-    <li>
-      <img
-        class="book_image"
-        src="https://leibniz.zbkb.nl/assets/id/PPN%3A44175127X?aid=ob-oba-lokaal-waas&sid=14&dts=1743610574120&sig=30745bc28a13c68c33c1586fda7e670da0d0e555cbfb425c5686ccfd99db9685&width=100"
-        alt=""
-      />
-      <div>
-        <a class="book_title" href="/">The women</a>
-        <span class="author">Kristin Hannah</span>
-      </div>
-    </li>
+  <li>
+    <img
+      class="book_image"
+      src="https://leibniz.zbkb.nl/assets/id/PPN%3A44175127X?aid=ob-oba-lokaal-waas&sid=14&dts=1743610574120&sig=30745bc28a13c68c33c1586fda7e670da0d0e555cbfb425c5686ccfd99db9685&width=100"
+      alt=""
+    />
+    <div>
+      <a class="book_title" href="/">The women</a>
+      <span class="author">Kristin Hannah</span>
+    </div>
+  </li>
 
-    <li>
-      <img
-        class="book_image"
-        src="https://leibniz.zbkb.nl/assets/id/PPN%3A443080828?aid=ob-oba-lokaal-waas&sid=14&dts=1743610574120&sig=d65df853721fa89d6778c6d2d8fbb272b7599da68a740e99bae7a2f91e6dfedf&width=100"
-        alt=""
-      />
-      <div>
-        <a class="book_title" href="/"
-          >Eekhoorn en de verborgen schat</a
-        >
-        <span class="author">Coralie Bickford-Smith</span>
-      </div>
-    </li>
+  <li>
+    <img
+      class="book_image"
+      src="https://leibniz.zbkb.nl/assets/id/PPN%3A443080828?aid=ob-oba-lokaal-waas&sid=14&dts=1743610574120&sig=d65df853721fa89d6778c6d2d8fbb272b7599da68a740e99bae7a2f91e6dfedf&width=100"
+      alt=""
+    />
+    <div>
+      <a class="book_title" href="/">Eekhoorn en de verborgen schat</a
+      >
+      <span class="author">Coralie Bickford-Smith</span>
+    </div>
+  </li>
 
-    <li>
-      <img
-        class="book_image"
-        src="https://leibniz.zbkb.nl/assets/id/PPN%3A444199365?aid=ob-oba-lokaal-waas&sid=14&dts=1743610574120&sig=9dd7328493e7fcd83ec08fabede40939d7ab01a8882743812ef236b39beecc2a&width=100"
-        alt=""
-      />
-      <div>
-        <a class="book_title" href="/">Togo</a>
-        <span class="author">Fiona Rempt</span>
-      </div>
-    </li>
+  <li>
+    <img
+      class="book_image"
+      src="https://leibniz.zbkb.nl/assets/id/PPN%3A444199365?aid=ob-oba-lokaal-waas&sid=14&dts=1743610574120&sig=9dd7328493e7fcd83ec08fabede40939d7ab01a8882743812ef236b39beecc2a&width=100"
+      alt=""
+    />
+    <div>
+      <a class="book_title" href="/">Togo</a>
+      <span class="author">Fiona Rempt</span>
+    </div>
+  </li>
 
-    <li>
-      <img
-        class="book_image"
-        src="https://leibniz.zbkb.nl/assets/id/PPN%3A444265554?aid=ob-oba-lokaal-waas&sid=14&dts=1743638505452&sig=f6f04284160bae51164e5a1eaeca0d7a6b30d15056b2bd23efab410d3ec835b0&width=100"
-        alt=""
-      />
-      <div>
-        <a class="book_title" href="/">Vuur en nuance</a>
-        <span class="author">Marjolein Moorman | Rutger</span>
-      </div>
-    </li>
+  <li>
+    <img
+      class="book_image"
+      src="https://leibniz.zbkb.nl/assets/id/PPN%3A444265554?aid=ob-oba-lokaal-waas&sid=14&dts=1743638505452&sig=f6f04284160bae51164e5a1eaeca0d7a6b30d15056b2bd23efab410d3ec835b0&width=100"
+      alt=""
+    />
+    <div>
+      <a class="book_title" href="/">Vuur en nuance</a>
+      <span class="author">Marjolein Moorman | Rutger</span>
+    </div>
+  </li>
 
-    <li>
-      <img
-        class="book_image"
-        src="https://leibniz.zbkb.nl/assets/id/PPN%3A443940975?aid=ob-oba-lokaal-waas&sid=14&dts=1743638505452&sig=3fff80eac3e6519b700263d833ad5f5aabbecad7d008f7c855024cdd5c56f13c&width=100"
-        alt=""
-      />
-      <div>
-        <a class="book_title" href="/">Juice</a>
-        <span class="author">Tim Winton</span>
-      </div>
-    </li>
-  </ul>
+  <li>
+    <img
+      class="book_image"
+      src="https://leibniz.zbkb.nl/assets/id/PPN%3A443940975?aid=ob-oba-lokaal-waas&sid=14&dts=1743638505452&sig=3fff80eac3e6519b700263d833ad5f5aabbecad7d008f7c855024cdd5c56f13c&width=100"
+      alt=""
+    />
+    <div>
+      <a class="book_title" href="/">Juice</a>
+      <span class="author">Tim Winton</span>
+    </div>
+  </li>
+</ul>
 
-  <style>
+<style>
   .books_container {
     list-style: "";
     display: flex;
-    overflow-x: scroll;
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
     gap: 1rem;
     margin-bottom: 2.5rem;
-    padding-bottom: 1.5rem;
+    anchor-name: --carousel;
 
     & li {
       display: flex;
       flex-flow: column;
+      scroll-snap-align: center;
       max-width: 10rem;
       gap: 0.5rem;
 
@@ -112,5 +112,4 @@
     object-fit: cover;
     border-radius: 0.5rem;
   }
-
-  </style>
+</style>
